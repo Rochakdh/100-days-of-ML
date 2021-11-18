ef5b80ca5cc91ac107c00b353734562867dd66b9
Rochakdh <rochakdh@gmail.com>
2021-11-18 19:40:32 +0545

    project breakdown

--- t-sne/README.md
+++ t-sne/README.md
@@ -1,3 +1,11 @@
 ![dataset] (https://github.com/Rochakdh/100-days-of-ML/blob/master/t-sne/MNIST-single-data.png?raw=true "fig :single_MNIST data set")
 
 ![t-sne] (https://github.com/Rochakdh/100-days-of-ML/blob/master/t-sne/t-sne_visualization.png?raw=true "t-SNE visualization of MNIST dataset")
+
+Things learned :
+1. t-sne is not a classifier or any algorithm to make clusters
+2. t-sne is a visualizer of high dimensional data.
+3. Generally it converts hyper dimension to 2-D for better visualization of data
+4. t-sne converts distances from each neighbour to probabilistic values.
+5. if they fall under a normal t-region, they are pulled together. If not the points are pushed
+6. In this project even though a minst is a supervised classification problem, we visualized data in an unsupervised way.
--- t-sne/tsne.py
+++ t-sne/tsne.py
@@ -1,33 +1,39 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
+import seaborn as sns #python visualization library based on matplotlib
 
-from sklearn import datasets
-from sklearn import manifold
+from sklearn import datasets #ml library, rigid than tf and pytorch, less customizable
+from sklearn import manifold #for non linear dimensionality reduction
 
 data = datasets.fetch_openml(
-    'mnist_784',
-    version=1,
-    return_X_y=True
+    'mnist_784', #name of data set
+    version=1, #version of data set
+    return_X_y=True #if true return tuple (data,target),else return bunch object(dict. like objet)
 )
 
-pixel_values, targets = data
+pixel_values, targets = data  # (pandas dataframe, pandas series)
 targets = targets.astype(int)
 
 single_image = pixel_values.iloc[1, :].values.reshape(28, 28)
 plt.imshow(single_image, cmap='gray')
 plt.show()
 
+
 tsne = manifold.TSNE(n_components=2, random_state=42)
+#n_comopenent is Dimension of the embedded space, lower dimension that we want the high dimension data to be converted to
 
 transformed_data = tsne.fit_transform(pixel_values.iloc[:3000, :])
+
 tsne_df = pd.DataFrame(
     np.column_stack((transformed_data, targets[:3000])),
     columns=["x", "y", "targets"]
-)
+) # here x and y is are components after t-sne decomposition. Result is a dataframe
 tsne_df.loc[:, "targets"] = tsne_df.targets.astype(int)
-grid = sns.FacetGrid(tsne_df, hue="targets", height=8)
+grid = sns.FacetGrid(tsne_df, hue="targets", height=40)
+
+""" Hue variable as a third dimension along a depth axis, where different 
+levels are plotted with different colors. Height represents the each facet height in inch"""
+
 grid.map(plt.scatter, "x", "y").add_legend()
 plt.show()
-
