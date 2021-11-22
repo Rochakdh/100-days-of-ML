ba450d735c029398f57ba1d01e1a8765c97813ac
Rochakdh <rochakdh@gmail.com>
2021-11-22 14:18:44 +0545

    testing differemt algorithm

--- arranging-ml-project/src/train.py
+++ arranging-ml-project/src/train.py
@@ -4,9 +4,12 @@ from sklearn import tree
 from sklearn import metrics
 
 import config
+import argparse
+import model_dispatcher
+import os
 
 
-def run(fold):
+def run(fold,model):
     df = pd.read_csv(config.TRAINING_FILE)
     df_train = df[df.kfold != fold].reset_index(drop=True)
     df_valid = df[df.kfold == fold].reset_index(drop=True)
@@ -17,19 +20,28 @@ def run(fold):
     x_valid = df_valid.drop("label", axis=1).values
     y_valid = df_valid.label.values
 
-    clf = tree.DecisionTreeClassifier()
+    clf = model_dispatcher.models[model]
     clf.fit(x_train, y_train)
 
     preds = clf.predict(x_valid)
     accuracy = metrics.accuracy_score(y_valid, preds)
     print(f"Fold={fold},Accuracy ={accuracy}")
 
-    joblib.dump(clf, f"{config.MODEL_OUTPUT}modelsdt_{fold}.bin")
+    joblib.dump(clf, os.path.join(config.MODEL_OUTPUT, f"modelsdt_{fold}.bin"))
 
 
 if __name__ == "__main__":
-    run(0)
-    run(1)
-    run(2)
-    run(3)
-    run(4)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--fold",
+        type=int
+    )
+    parser.add_argument(
+        "--model",
+        type=str
+    )
+    args = parser.parse_args()
+    run(
+        fold=args.fold,
+        model = args.model
+    )
