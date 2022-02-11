a1451c005c6a254aaaaaf21655069ebf33c196c8
Rochakdh <rochakdh@gmail.com>
2022-02-11 10:10:37 +0545

    class mode edited and train model changed

--- dog-cat-classifications-cnn/src/config.py
+++ dog-cat-classifications-cnn/src/config.py
@@ -1,3 +1,3 @@
-TRAIN_DIRECTORY= "../input/dogscats/train/"         #23,002 items
-VALIDATION_DIRECTORY= ".../input/dogscats/valid/"   #1000 items
+TRAIN_DIRECTORY= "../input/dogscats/train/"         #23,000 items 11500 each dog and cat
+VALIDATION_DIRECTORY= "../input/dogscats/valid/"   #2000 items 1000 items each
 MODEL_OUTPUT = "../models/"
--- dog-cat-classifications-cnn/src/train.py
+++ dog-cat-classifications-cnn/src/train.py
@@ -1,24 +1,50 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.optimizers import RMSprop
 import config
 
 def generator():
-    train_datgen = ImageDataGenerator(rescale=1.0/255)
+    train_datgen = ImageDataGenerator(rescale=1./255)
 
     train_genrator = train_datgen.flow_from_directory(
         config.TRAIN_DIRECTORY,
         target_size=(256, 256),
         color_mode='rgb',
-        batch_size=128,
+        batch_size=100,
+        shuffle=True,
+        class_mode='binary'
     )
 
     validation_genrator = train_datgen.flow_from_directory(
         config.VALIDATION_DIRECTORY,
         target_size=(256, 256),
         color_mode='rgb',
-        batch_size=32,
+        batch_size=50,
+        shuffle=True,
+        class_mode='binary'
     )
+    return train_genrator,validation_genrator
+
+def model():
+    model = tf.keras.models.Sequential(
+        [
+            tf.keras.layers.Conv2D(16,(3,3),activation='relu',input_shape=(256,256,3)),
+            tf.keras.layers.MaxPool2D(2,2),
+            tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+            tf.keras.layers.MaxPool2D(2, 2),
+            tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+            tf.keras.layers.MaxPool2D(2, 2),
+            tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPool2D(2, 2),
+            tf.keras.layers.Conv2D(16, (3, 3), activation='relu'),
+            tf.keras.layers.MaxPool2D(2, 2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(512,activation='relu'),
+            tf.keras.layers.Dense(1,activation='sigmoid')
+        ]
+    )
+    return model
 
 class myCallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
@@ -27,4 +53,17 @@ class myCallback(tf.keras.callbacks.Callback):
             self.model.stop_training = True
 
 if __name__ == '__main__':
-    generator()
+    callbacks = myCallback()
+    train_generator,validation_generator = generator()
+    model_used = model()
+    model_used.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['acc'])
+    model_used.fit_generator(
+        train_generator,
+        steps_per_epoch=115,
+        epochs=20,
+        validation_data=validation_generator,
+        validation_steps=20,
+        callbacks=[callbacks]
+    )
+    model_used.save_weights(f'{config.MODEL_OUTPUT}/model.h5')
+    print(model_used.summary())
